24d0cfaf50e2542393f72d7c781ef9fc4a00602d
D'Jreya Boyd <37115281+DJreyaB@users.noreply.github.com>
2025-04-25 14:24:43 -0600

    player added to scene
    build out of player walking with animation

--- 2d-project-start 2/player.gd
+++ 2d-project-start 2/player.gd
@@ -2,4 +2,12 @@ extends CharacterBody2D
 
 
 func _physics_process(delta: float) -> void:
-	pass
+	#setting it up to get location
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity = direction * 600 #base variable, no need to define its an exisitng variable, this is an update
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		%HappyBoo.play_walk_animation()
+	else:
+		%HappyBoo.play_idle_animation()
