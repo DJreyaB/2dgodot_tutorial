8b4d1c3496f1f65b72d0697642c3a2eb95d70aad
D'Jreya Boyd <37115281+DJreyaB@users.noreply.github.com>
2025-04-25 14:51:14 -0600

    fundamental mechanics added
    added mobs that follow player, camera to move around as player does, renamed pinetree nodes

--- 2d-project-start 2/mob_enemy.gd
+++ 2d-project-start 2/mob_enemy.gd
@@ -2,3 +2,9 @@ extends CharacterBody2D
 
 
 #needs a reference for the player to allow for player following
+@onready var player = get_node("/root/Game/Player")
+
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 300
+	move_and_slide()
